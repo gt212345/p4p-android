1183e0a8035ce32935fc5d84b628b952c2593722
gt212345 <wuheiru.5203@gmail.com>
2014-05-25 00:18:16 +0800

    Add welcome animation

--- p4p-android/src/info/androidhive/slidingmenu/WelcomeFragment.java
+++ p4p-android/src/info/androidhive/slidingmenu/WelcomeFragment.java
@@ -5,17 +5,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class WelcomeFragment extends Fragment {
-	
-	public WelcomeFragment(){}
-	
+
+	public WelcomeFragment() {
+	}
+
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
- 
-        View rootView = inflater.inflate(R.layout.fragment_find_people, container, false);
-         
-        return rootView;
-    }
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_welcome, container,
+				false);
+
+		return rootView;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+		TextView welcome = (TextView) getActivity().findViewById(R.id.welcome);
+		welcome.startAnimation(AnimationUtils.loadAnimation(getActivity(),
+				R.anim.animate_welcome));
+	}
 }
